7ddca245d62c99422d048af9668f74bada359629
formacio <formacio@M022662>
2019-07-02 10:58:42 +0200

    new commit

---
+++ src/main/java/com/whitecollar/gallery/WhiteCollarGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.whitecollar.gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteCollarGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteCollarGalleryApplication.class, args);
+	}
+
+}
